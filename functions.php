ad0f3c8e060399ee0e5c4c670b88d88fbc7ac916
Kate Jeziorska <k_jeziorska@yahoo.com>
2018-05-30 10:55:31 -0700

    create js file and add js localize function in functions.php

--- functions.php
+++ functions.php
@@ -72,6 +72,17 @@ function qod_scripts() {
 	/**
 	 * @TODO add localize script rest api JavaScript
 	 */
+	if(function_exists('rest_url') ){
+		wp_enqueue_script( 'qod_api', get_template_directory_uri() . '/build/js/api.min.js', array('jquery'), false, true);
+		wp_localize_script( 'qod_api', 'api_vars', array(
+			'root_url' => esc_url_raw(rest_url()),
+			'home_url'=> esc_url_raw(home_url()),
+			'nonce'=> wp_create_nonce('wp_rest'),
+			'success'=>'Thanks, your quote submission was received',
+			'failure'=>'Sorry, your submission could not be processed',
+		));
+	}
+
 }
 add_action( 'wp_enqueue_scripts', 'qod_scripts' );
 
--- js/api.js
+++ js/api.js
@@ -2,15 +2,38 @@
   'use strict';
 
   // remove this body append code, just for initial test
-  $('body').append('js file is working');
+  // $('body').append('js file is working');
 
   /**
    * Ajax-based random post fetching & History API
    */
+  $('#new-quote-button').on('click', function(e){
+    e.preventDefault();
+    // console.log('click');
+    $.ajax({
+      method:'get',
+      url:api_vars.root_url + 'wp/v2/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+      cache:false,
+    }).done(function(data){
+      console.log(data);
+
+//append the data to html, look at content.php 
+
+
+
+    }).fail(function(){
+
+      return 'There was an error';
+
+    });
+
+  });
+
 
 
   /**
    * Ajax-based front-end post submissions.
    */
+//take a look at the wp javascripts slides for post request, also in readsprout theme
 
 })(jQuery);
