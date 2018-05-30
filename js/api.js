a600ae677e6ee35f81f9f9b3cbff8664a5077f92
Kate Jeziorska <k_jeziorska@yahoo.com>
2018-05-30 13:24:19 -0700

    write get ajax method

--- js/api.js
+++ js/api.js
@@ -1,25 +1,38 @@
 (function($) {
-  'use strict';
-
-  // remove this body append code, just for initial test
-  // $('body').append('js file is working');
-
-  /**
-   * Ajax-based random post fetching & History API
-   */
+  // 'use strict';
+  //before the button click
+  var lastPage = '';
   $('#new-quote-button').on('click', function(e){
     e.preventDefault();
-    // console.log('click');
+
+    //button click update the lastPage before the ajax request
+    lastPage = document.URL;
+
     $.ajax({
       method:'get',
-      url:api_vars.root_url + 'wp/v2/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+      url:api_vars.root_url + 'wp/v2/posts?filter[orderby]=rand&filter[data[0]s_per_page]=1',
       cache:false,
+
     }).done(function(data){
       console.log(data);
 
-//append the data to html, look at content.php 
+        //History Push State in get request try looking wp slug, should update the url with the slug
+        history.pushState(null, null, data[0].slug);
+      //append the data to html, look at content.php
 
 
+        //replace the current quote with the new quote
+          $('.entry-content').html( data[0].content.rendered );
+          $( '.entry-title' ).html( '&mdash; ' + data[0].title.rendered );
+          if ( data[0]._qod_quote_source !== '' && data[0]._qod_quote_source_url !== ''){
+            $( '.source' ).html(' , ' + '<a href="' + data[0]._qod_quote_source_url+ '">' + data[0]._qod_quote_source + '</a>');
+          }
+          else if (data[0]._qod_quote_source !== '') {
+          $('.source').html(', ' + data[0]._qod_quote_source)
+         }
+          else {
+          $( '.source' ).html('');
+        }
 
     }).fail(function(){
 
@@ -29,11 +42,11 @@
 
   });
 
+//Widnow Pop State //when you click arrows
+        $(window).on('popstate', function(){
+          window.location.replace(lastPage);
+      })
 
-
-  /**
-   * Ajax-based front-end post submissions.
-   */
-//take a look at the wp javascripts slides for post request, also in readsprout theme
+//take a look at the wp javascripts slides for data[0] request, also in readsprout theme
 
 })(jQuery);
